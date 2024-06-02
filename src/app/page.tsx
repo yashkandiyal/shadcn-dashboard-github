6e6b49191a2a7a45e6f1a8187086f50889525ea9
Yash kandiyal <yashkandiyal22@gmail.com>
2024-06-02 16:39:15 +0530

    ui chanes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-"use client"
-import React, {useEffect, useState} from "react";
-import LoaderComponent from "@/components/Loader";
-
 const page = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return <LoaderComponent />;
-  }
   return <div></div>;
 };
 
