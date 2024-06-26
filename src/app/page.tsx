ace298aa02418da545de6e86f8d95962aab39c84
Yash kandiyal <yashkandiyal22@gmail.com>
2024-06-26 12:12:52 +0530

    middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-const page = () => {
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const page = async () => {
+  const { userId }: { userId: string | null } = auth();
+  const user = await currentUser();
+  console.log(userId, user);
+
   return <div></div>;
 };
 
