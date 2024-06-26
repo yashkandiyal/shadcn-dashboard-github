48d26fd90fdce6c717cd2449b987ece5c25ef265
Yash kandiyal <yashkandiyal22@gmail.com>
2024-06-26 12:16:36 +0530

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/dashboard(.*)",
+  "/",
   "/api/webhooks/stripe",
   "/api/webhooks/clerk",
   "/sign-in(.*)",
